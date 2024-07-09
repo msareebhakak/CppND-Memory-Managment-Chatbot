40dfdff4c3879e681e2f81aaf0a45181561bd782
Sareeb Hakak <Sareeb.Hakak@omron.com>
2024-07-09 00:46:29 -0400

    Task 1: use unique pointer to create _chatLogic

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -118,7 +118,9 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    _chatLogic = new ChatLogic(); 
+//    _chatLogic = new ChatLogic();
+    /// use std::make_unique instead of new for exception handling
+    _chatLogic = std::make_unique<ChatLogic>();
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
@@ -134,8 +136,8 @@ ChatBotPanelDialog::~ChatBotPanelDialog()
 {
     //// STUDENT CODE
     ////
-
-    delete _chatLogic;
+    /// No need to delete with unique pointer
+//    delete _chatLogic;
 
     ////
     //// EOF STUDENT CODE
--- src/chatgui.h
+++ src/chatgui.h
@@ -2,6 +2,7 @@
 #define CHATGUI_H_
 
 #include <wx/wx.h>
+#include <memory>
 
 class ChatLogic; // forward declaration
 
@@ -15,8 +16,9 @@ private:
 
     //// STUDENT CODE
     ////
-
-    ChatLogic *_chatLogic;
+    /// Change to unique pointer to get exclusive ownership
+//    ChatLogic *_chatLogic;
+    std::unique_ptr<ChatLogic> _chatLogic;
 
     ////
     //// EOF STUDENT CODE
@@ -27,7 +29,9 @@ public:
     ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+//    ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+    /// use .get() to get the raw pointer from unique pointer
+    ChatLogic *GetChatLogicHandle() { return _chatLogic.get(); }
 
     // events
     void paintEvent(wxPaintEvent &evt);
